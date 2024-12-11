f7aed7e0af82cf7e9d16725d32ab7e3bc95728c8
b1tzxrd <bitzxrdcoding@gmail.com>
2024-12-11 11:00:20 +0500

    Updated package.json and server.js for Heroku deployment

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
+// server.js
 import { create, router as _router, defaults, rewriter } from 'json-server';
 const server = create();
-const router = _router('./reviews.json');
+const router = _router('./db.json'); // путь к вашему файлу с данными
 const middlewares = defaults({
-    static: './build'
+    static: './dist'
 });
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 8000;
+
 server.use(middlewares);
 server.use(rewriter({
     '/api/*': '/$1',
-}))
+}));
 server.use(router);
+
 server.listen(PORT, () => {
     console.log('Server is running');
 });
-
